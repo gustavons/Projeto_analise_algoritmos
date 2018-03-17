982078d050dc86a5e374a71e8e0c7d72d7ec6ca4
Douglas Laurindo <douglaslaurindo.stm@hotmail.com>
2018-03-16 21:04:48 -0300

    Kadane atualizado de acordo com o pseudocodigo

--- src/Kadane.java
+++ src/Kadane.java
@@ -1,19 +1,15 @@
 public class Kadane {
 
-    public int solve(int a[])
-    {
-        int size = a.length;
-        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+    double maximoSubArranjo(double x[]) {
+        double maxAteAgora = 0;
+        double maxFinalAqui = 0;
 
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < x.length; i++)
         {
-            max_ending_here = max_ending_here + a[i];
-            if (max_so_far < max_ending_here)
-                max_so_far = max_ending_here;
-            if (max_ending_here < 0)
-                max_ending_here = 0;
+            maxFinalAqui = Math.max(0.0, (maxFinalAqui + x[i]));
+            maxAteAgora = Math.max(maxAteAgora, maxFinalAqui);
         }
-        return max_so_far;
+        return maxAteAgora;
     }
 
 }
